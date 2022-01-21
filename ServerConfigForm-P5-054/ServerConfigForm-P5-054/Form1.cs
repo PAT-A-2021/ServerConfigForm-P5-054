ff7cda0b80b69f0942d9d84efe026f5776fea708
ASUS <moh.sigit0001@gmail.com>
2022-01-22 01:53:54 +0700

    Add project files.

---
+++ ServerConfigForm-P5-054/ServerConfigForm-P5-054/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            labelOnOrOff.Text = " Server ON ";
+            labelKet.Text = " Klik OFF untuk Mematikan Server ";
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            labelKet.Text = " Klik ON untuk Menjalankan Server ";
+            labelOnOrOff.Text = " Server OFF ";
+        }
+    }
+}
